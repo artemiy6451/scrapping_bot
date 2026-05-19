3e2d574e134637fc326807aab476171033615a1a
Kazantsev Artem <me@lokach.ru>
2026-05-19 19:11:26 +0300

    add: start command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
-def main(name: str) -> None:
-    print(f"Hello {name}!")
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config import settings
+from routers.start import start_router
+
+dp = Dispatcher()
+dp.include_router(start_router)
+
+
+async def main() -> None:
+    print(settings.telegram_bot_token)
+    bot = Bot(
+        token=settings.telegram_bot_token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main("world")
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
